4b0be5e7d5904ba04e127424077473dda09040ad
Lucas Denipoti <lucasfdenipoti@gmail.com>
2024-02-20 12:17:45 -0300

    comit final

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: 'Olá Mundo!!!'
-    end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do 
+  resources :bubbles
   root 'pages#home'
-  get 'about', to: 'pages#about'
 end
